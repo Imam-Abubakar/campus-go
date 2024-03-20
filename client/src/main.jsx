a7ccca396a3fc25309865e3f7611e00d7477cfa2
Imam Abubakar <imamabubakar2002@gmail.com>
2024-03-20 08:29:54 +0000

    (update): PWA Prompt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./context/AuthProvider.jsx";
 import UserProvider from "./context/UserProvider.jsx";
+import PWAPrompt from "react-ios-pwa-prompt";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -16,5 +17,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </UserProvider>
       </AuthProvider>
     </BrowserRouter>
+    <PWAPrompt
+      promptOnVisit={1}
+      timesToShow={10}
+      copyClosePrompt="Close"
+      permanentlyHideOnDismiss={false}
+    />
   </React.StrictMode>
 );
